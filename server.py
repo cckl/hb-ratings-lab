11eb50b8d714d19927093a8bc5a03ac4cbd40f9e
Hackbright Student <no-reply@hackbrightacademy.com>
2019-02-05 18:00:07 -0800

    Finish user registration route

---
+++ server.py
@@ -0,0 +1,67 @@
+"""Movie Ratings."""
+
+from jinja2 import StrictUndefined
+
+from flask import (Flask, render_template, redirect, request, flash,
+                   session)
+from flask_debugtoolbar import DebugToolbarExtension
+
+from model import User, Rating, Movie, connect_to_db, db
+
+app = Flask(__name__)
+
+# Required to use Flask sessions and the debug toolbar
+app.secret_key = "ABC"
+
+# Normally, if you use an undefined variable in Jinja2, it fails
+# silently. This is horrible. Fix this so that, instead, it raises an
+# error.
+app.jinja_env.undefined = StrictUndefined
+
+
+@app.route('/')
+def index():
+    """Homepage."""
+    return render_template("homepage.html")
+
+
+@app.route("/users")
+def user_list():
+    """Show list of users."""
+
+    users = User.query.all()
+    return render_template("user_list.html", users=users)
+
+@app.route('/register')
+def register_form():
+    """Homepage."""
+    return render_template("register_form.html")
+
+@app.route('/register', methods=['POST'])
+def register_process():
+    """Homepage."""
+
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if User.query.filter(User.email == email).one():
+        # add action for checking password for existing user
+    else:
+        db.session.add(User(email, password))
+        db.session.commit()
+
+    return redirect("/")
+
+if __name__ == "__main__":
+    # We have to set debug=True here, since it has to be True at the
+    # point that we invoke the DebugToolbarExtension
+    app.debug = True
+    # make sure templates, etc. are not cached in debug mode
+    app.jinja_env.auto_reload = app.debug
+
+    connect_to_db(app)
+
+    # Use the DebugToolbar
+    DebugToolbarExtension(app)
+
+    app.run(port=5000, host='0.0.0.0')
